bbb5bf3315c7c2e986dcdb6ea5d1cd09feee4934
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2022-02-21 21:37:27 -0600

    WIP update page

--- pages/create.js
+++ pages/create.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Row, Col, Form, Button, Alert } from "react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Nav from "../components/Nav"
+import Nav from "../components/Nav";
 
 const testIssue = {
   title: "First issue",
@@ -10,7 +10,7 @@ const testIssue = {
   user: "Joey",
   date: Date(),
   tags: ["test", "database"],
-  resolved: false
+  resolved: false,
 };
 
 export default function Create({ issues }) {
@@ -30,14 +30,14 @@ export default function Create({ issues }) {
         user: "Joey",
         date: Date(),
         tags: ["test", "database"],
-        resolved: false
+        resolved: false,
       }),
     });
     res = await res.json();
     setIssueTitle("");
     setIssueText("");
     setSubmitted(true);
-    e.target.reset()
+    e.target.reset();
   };
 
   const handleIssueTitleChange = (e) => {
@@ -54,57 +54,66 @@ export default function Create({ issues }) {
 
   return (
     <>
-    <Nav />
-      {!submitted ? 
-          <Form onSubmit={handleSubmit} style={{margin: "8em"}}>
-        <Form.Group className="mb-3" controlId="createFormTitle">
-          <Form.Label>Issue Title</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={1}
-            onChange={handleIssueTitleChange}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="createFormText">
-          <Form.Label>Issue Description</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            onChange={handleIssueTextChange}
-          />
-        </Form.Group>
-        <fieldset>
-          <Form.Group as={Row} className="mb-3" onChange={handlePriorityChange}>
-            <Form.Label as="legend" column sm={2}>
-              Priority Level
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Check
-                type="radio"
-                label="High"
-                value="High"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios1"
-              />
-              <Form.Check
-                type="radio"
-                label="Medium"
-                value="Medium"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios2"
-              />
-              <Form.Check
-                type="radio"
-                label="Low"
-                value="Low"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios3"
-              />
-            </Col>
+      <Nav />
+      {!submitted ? (
+        <Form onSubmit={handleSubmit} style={{ margin: "8em" }}>
+          <Form.Group className="mb-3" controlId="createFormTitle">
+            <Form.Label>Issue Title</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={1}
+              onChange={handleIssueTitleChange}
+            />
           </Form.Group>
-        </fieldset>
-        <Button type="submit">Submit Issue</Button>
-      </Form> : <Alert style={{margin: "8em"}} variant="success">Issue submitted!</Alert>}
+          <Form.Group className="mb-3" controlId="createFormText">
+            <Form.Label>Issue Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              onChange={handleIssueTextChange}
+            />
+          </Form.Group>
+          <fieldset>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              onChange={handlePriorityChange}
+            >
+              <Form.Label as="legend" column sm={2}>
+                Priority Level
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="radio"
+                  label="High"
+                  value="High"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios1"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Medium"
+                  value="Medium"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios2"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Low"
+                  value="Low"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios3"
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
+          <Button type="submit">Submit Issue</Button>
+        </Form>
+      ) : (
+        <Alert style={{ margin: "8em" }} variant="success">
+          Issue submitted!
+        </Alert>
+      )}
     </>
   );
 }
--- pages/view.js
+++ pages/view.js
@@ -1,14 +1,91 @@
-import { Card, Button, Row, Col, Alert } from "react-bootstrap";
+import { useState } from "react";
+import { Card, Button, Row, Col, Alert, Modal, Form } from "react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Nav from "../components/Nav"
-
+import Nav from "../components/Nav";
 
 export default function View({ issues }) {
+  const [show, setShow] = useState(false);
+  const [selectedIssue, setSelectedIssue] = useState(null);
+  const handleClose = () => setShow(false);
+  const handleShow = (issue) => {
+    setShow(true);
+    setSelectedIssue(issue);
+  };
   return (
     <>
-    <Nav />
+      <Modal show={show} onHide={handleClose} backdrop="static" size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Update Issue</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={() => console.log("click")} style={{ margin: "8em" }}>
+            <Form.Group className="mb-3" controlId="createFormTitle">
+              <Form.Label>Issue Title</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={1}
+                value={selectedIssue?.title}
+                onChange={() => console.log("click")}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="createFormText">
+              <Form.Label>Issue Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                value={selectedIssue?.content}
+                onChange={() => console.log("click")}
+              />
+            </Form.Group>
+            <fieldset>
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                onChange={() => console.log("click")}
+              >
+                <Form.Label as="legend" column sm={2}>
+                  Priority Level
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Check
+                    type="radio"
+                    label="High"
+                    value="High"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios1"
+                    checked={selectedIssue?.priority === "High"}
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Medium"
+                    value="Medium"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios2"
+                    checked={selectedIssue?.priority === "Medium"}
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Low"
+                    value="Low"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios3"
+                    checked={selectedIssue?.priority === "Low"}
+                  />
+                </Col>
+              </Form.Group>
+            </fieldset>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+      <Nav />
       {issues.data.length > 0 ? (
-        <Row xs={1} md={2} className="g-4" style={{margin: "8em"}}>
+        <Row xs={1} md={2} className="g-4" style={{ margin: "8em" }}>
           {issues.data.map((issue) => (
             <Col>
               <Card>
@@ -17,14 +94,16 @@ export default function View({ issues }) {
                   <Card.Text>{issue.content}</Card.Text>
                   <Card.Text>{issue.user}</Card.Text>
                   <Card.Text>{issue.date}</Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="primary" onClick={() => handleShow(issue)}>
+                    Update
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
           ))}
         </Row>
       ) : (
-        <Alert style={{margin: "8em"}}>No issues!</Alert>
+        <Alert style={{ margin: "8em" }}>No issues!</Alert>
       )}
     </>
   );
