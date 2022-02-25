dcae79ba3c41555c6d9c085dbbb10a98b1f95249
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2022-02-24 21:45:58 -0600

    Get only issues from current user

--- pages/api/issues/[pid].js
+++ pages/api/issues/[pid].js
@@ -1,4 +1,10 @@
-export default function handler(req, res) {
+import clientPromise from "../../../lib/mongodb";
+var ObjectId = require('mongodb').ObjectId;
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("issue_tracker_db");
   const { pid } = req.query;
-  res.status(200).json({ name: pid })
+  const issues = await db.collection("issues").find({user: pid}).toArray();
+  res.status(200).json({ data: issues })
 }
